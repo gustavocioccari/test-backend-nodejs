d26a6ece670c3ad6c78736eab054b750816b271f
gustavocioccari <gustavocioccari@gmail.com>
2021-02-24 20:06:59 -0300

    Implement product create

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -2,10 +2,18 @@ const Product = require('../Models/Product')
 
 module.exports = {
   async create(request, response){
+    const { title,description,price,category } = request.body
+    
     try {
+      if (await Product.findOne({ title }))
+        return response.status(400).send({ error:'Category already exists' })
+
+      const product = await Product.create(request.body)
+
+      return response.json({ product })
 
     }catch(err){
-      return response.status(400).send({ error: 'Product registration has failed' });
+      return response.status(400).send({ error: 'Product registration has failed' }), console.log(err)
     }
   },
 
