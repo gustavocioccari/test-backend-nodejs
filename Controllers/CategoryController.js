c6e3eb63342b560f75b9966624a856ba09237e4a
gustavocioccari <gustavocioccari@gmail.com>
2021-02-24 18:15:07 -0300

    Implement category create

--- Controllers/CategoryController.js
+++ Controllers/CategoryController.js
@@ -2,10 +2,17 @@ const Category = require('../Models/Category')
 
 module.exports = {
   async create(request, response){
+    var { category } = request.body
+    
     try {
+      if (await Category.findOne({ category }))
+        return response.status(400).send({ error:'Category already exists' })
       
+      category = await Category.create(request.body)
+
+      return response.json({ category })
     }catch(err){
-      return response.status(400).send({ error: 'Category registration has failed' });
+      return response.status(400).send({ error: 'Category registration has failed' }), console.log(err)
     }
   },
 }
